a0ecac4d409a526a1f3b2a241be855ad310defad
mstfky-makira <mustafa.kaya@makira.io>
2025-03-16 21:55:21 -0400

    Refactored RbSerializerTest to remove duplicate logic and improve maintainability - Group 12

--- robocode.core/src/test/java/net/sf/robocode/serialization/RbSerializerTest.java
+++ robocode.core/src/test/java/net/sf/robocode/serialization/RbSerializerTest.java
@@ -56,7 +56,7 @@ public class RbSerializerTest {
 		ec.setBodyTurnRemaining(150.123);
 		ec.setTryingToPaint(true);
 
-		ExecCommands ec2 = serializeAndDeserializeExecCommands(ec);
+		ExecCommands ec2 = serializeAndDeserialize(ec, RbSerializer.ExecCommands_TYPE);
 
 		assertNear(ec2.getBodyTurnRemaining(), ec.getBodyTurnRemaining());
 		Assert.assertTrue(ec2.isTryingToPaint());
@@ -70,7 +70,7 @@ public class RbSerializerTest {
 		ec.getBullets().add(new BulletCommand(1.0, false, 0.9454, 12));
 		ec.getBullets().add(new BulletCommand(1.0, true, 0.9554, -128));
 
-		ExecCommands ec2 = serializeAndDeserializeExecCommands(ec);
+		ExecCommands ec2 = serializeAndDeserialize(ec, RbSerializer.ExecCommands_TYPE);
 
 		assertNear(ec2.getBodyTurnRemaining(), ec.getBodyTurnRemaining());
 		assertNear(ec2.getBullets().get(0).getPower(), 1.0);
@@ -90,7 +90,7 @@ public class RbSerializerTest {
 		ec.getTeamMessages().add(new TeamMessage("Foo", "Bar", data));
 		ec.getTeamMessages().add(new TeamMessage("Foo", "Bar", null));
 
-		ExecCommands ec2 = serializeAndDeserializeExecCommands(ec);
+		ExecCommands ec2 = serializeAndDeserialize(ec, RbSerializer.ExecCommands_TYPE);
 
 		Assert.assertEquals(ec2.getTeamMessages().get(0).message[0], 0);
 		Assert.assertEquals(ec2.getTeamMessages().get(0).message[10], 10);
@@ -107,74 +107,19 @@ public class RbSerializerTest {
 		ec.getTeamMessages().add(new TeamMessage("Foo", "Bar", null));
 		ec.getDebugProperties().add(new DebugProperty("UTF8 Native characters", "Příliš žluťoučký kůň úpěl ďábelské ódy."));
 
-		ExecCommands ec2 = serializeAndDeserializeExecCommands(ec);
+		ExecCommands ec2 = serializeAndDeserialize(ec, RbSerializer.ExecCommands_TYPE);
 
 		Assert.assertEquals(ec2.getDebugProperties().get(0).getKey(), "UTF8 Native characters");
 		Assert.assertEquals(ec2.getDebugProperties().get(0).getValue(), "Příliš žluťoučký kůň úpěl ďábelské ódy.");
 	}
 
-	@Test
-	public void graphics() throws InterruptedException {
-		final Graphics2DSerialized sg = new Graphics2DSerialized();
-
-		sg.setPaintingEnabled(true);
-		sg.setBackground(Color.GREEN);
-		sg.setColor(Color.RED);
-		sg.draw(new Arc2D.Double(Arc2D.PIE));
-
-		sg.setColor(Color.BLUE);
-		sg.draw(new Line2D.Double(99, 98, 78, 3));
-
-		sg.setColor(Color.YELLOW);
-		sg.draw(new Rectangle2D.Double(20, 20, 30, 50));
-
-		sg.setColor(Color.BLACK);
-		sg.drawLine(99, 3, 78, 3);
-		sg.drawRect(90, 20, 30, 50);
-
-		sg.setColor(Color.CYAN);
-		sg.setStroke(new BasicStroke(1, 2, BasicStroke.JOIN_ROUND, 4, null, 0));
-		sg.fill(new Rectangle2D.Double(20, 70, 30, 50));
-		sg.fill(new Ellipse2D.Double(70, 70, 30, 50));
-
-		sg.setColor(Color.MAGENTA);
-		sg.fill(new RoundRectangle2D.Double(110, 70, 30, 50, 13.5, 16.1));
-
-		exception = null;
-
-		Canvas d = new Canvas() {
-			@Override
-			public void paint(Graphics g) {
-				synchronized (this) {
-					try {
-						sg.processTo((Graphics2D) g);
-					} catch (Exception e) {
-						exception = e;
-					}
-				}
-			}
-		};
-
-		d.setSize(200, 200);
-		if (!GraphicsEnvironment.isHeadless()) {
-			JFrame f = new JFrame();
-			f.add(d);
-			f.pack();
-			f.setVisible(true);
-			Thread.sleep(100);
-			f.setVisible(false);
-		}
-
-		Assert.assertNull("Exception occurred: " + exception, exception);
-	}
-
-	private ExecCommands serializeAndDeserializeExecCommands(ExecCommands ec) throws IOException {
+	private <T> T serializeAndDeserialize(T object, int type) throws IOException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream(1024);
 		RbSerializer rbs = new RbSerializer();
 
-		rbs.serialize(out, RbSerializer.ExecCommands_TYPE, ec);
+		rbs.serialize(out, type, object);
 		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
-		return (ExecCommands) rbs.deserialize(in);
+		return (T) rbs.deserialize(in);
 	}
 
 	public static void assertNear(double v1, double v2) {
