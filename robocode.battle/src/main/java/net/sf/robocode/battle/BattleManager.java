defe9825134054705427cff44a221cc1c693e93e
Divy <divysoni0089@gmail.com>
2025-03-17 21:21:47 -0400

    updated code little bit

--- robocode.battle/src/main/java/net/sf/robocode/battle/BattleManager.java
+++ robocode.battle/src/main/java/net/sf/robocode/battle/BattleManager.java
@@ -408,8 +408,8 @@ public class BattleManager implements IBattleManager {
 	}
 
 	public synchronized void setSGPaintEnabled(int robotIndex, boolean enable) {
-		if (battle != null && battle.isRunning() && !isPaused() && battle instanceof Battle battleInstance) {
-			battleInstance.sendInteractiveEvent(event);
+		if (battle != null && battle.isRunning() && battle instanceof Battle) {
+			((Battle) battle).setSGPaintEnabled(robotIndex, enable);
 		}
 	}
 
